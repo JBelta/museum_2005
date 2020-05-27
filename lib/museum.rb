e598a739158da6c451600e93847607ccd71d73f5
James Belta <james.belta@gmail.com>
2020-05-27 10:19:42 -0600

    Added recomended exhibits method

--- lib/museum.rb
+++ lib/museum.rb
@@ -9,4 +9,12 @@ class Museum
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
+
+  def recommend_exhibits(patron)
+    recomendations = []
+    recomendations << @exhibits.find_all do |exhibit|
+      patron.interest.any?(exhibit.name)
+    end
+    recomendations.flatten
+  end
 end
