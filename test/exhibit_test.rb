cbb0b7995a429aafad5b15178f7152c26a8060a3
James Belta <james.belta@gmail.com>
2020-05-27 09:25:34 -0600

    Build setup in test, instance test pass

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -5,8 +5,17 @@ require './lib/exhibit'
 
 class ExhibitTest < Minitest::Test
 
-  def test_it_exists
-    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
-    assert_instance_of Exhibit, exhibit 
+  def setup
+    @exhibit = Exhibit.new({
+                            name: "Gems and Minerals",
+                            cost: 0
+                            })
+    @patron_1 = Patron.new("Bob", 20)
   end
+
+  def test_it_exists
+    assert_instance_of Exhibit, @exhibit
+  end
+
+
 end
