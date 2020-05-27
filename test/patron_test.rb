9fcc6899d1f5e2d4ca503e24840cd4e011e8741b
James Belta <james.belta@gmail.com>
2020-05-27 09:09:46 -0600

    Build Attributes test

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -8,4 +8,12 @@ class PatronTest < Minitest::Test
     bob = Patron.new("Bob", 20)
     assert_instance_of Patron, bob
   end
+
+  def test_it_has_attributes
+    bob = Patron.new("Bob", 20)
+
+    assert_equal "Bob", bob.name
+    assert_equal 20, bob.spending_money
+    assert_equal [], bob.interest
+  end
 end
